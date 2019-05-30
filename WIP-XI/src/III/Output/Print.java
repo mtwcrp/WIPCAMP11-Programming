c092ee88203d3d7034f27e3b383eb8b0f490216c
mtwcrp <mintwankham@hotmail.com>
2019-05-30 11:38:12 +0700

    [add] new project

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("ชื่อมิ้นท์ ");
+        
+        System.out.print("อยู่มันม่วง \n");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("เพื่อนข้างๆชื่อ \n");
+        
+        System.out.print("โฉม ไข่มุก\n");
+        
+    }
+}
